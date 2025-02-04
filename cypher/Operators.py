581f528da6871287ccf138941c771151c8ddb4ac
VEXALIA <ruoroabigael@gmail.com>
2025-02-04 16:18:19 +0300

    Add files via upload

---
+++ cypher/Operators.py
@@ -0,0 +1,34 @@
+# Arithmetic Operators
+x = 10
+y = 5
+
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y) # Modulus - Returns the remainder
+
+# comparison Operaters
+print(x < y)
+print(x > y)
+print(x <= y)
+print(x >= y)
+print(x == y) # equal to
+print(x != y) # not equal to
+
+# Assigment Operators
+a = 30
+print(a)
+
+a -=2
+print(a)
+# operater Precedence
+number = (45 - 2 * 3 + 5)
+print(number)
+
+#Logical perators - and, or, not
+print(55 < 234 and 67 <23)
+print(55 < 234 or 67 < 23)
+print(not(55 < 234 or 67 < 23))
+
---
+++ cypher/variable.py
@@ -0,0 +1,20 @@
+# a varible is a container used to store a value
+
+location = "Westlands"
+firstname = "Abigael"
+lastname = "Cypher"
+school = "emobilis"
+x = 39
+y = 2
+k = 39
+m= 2
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname, lastname)
+print("My fullname is", firstname,lastname)
+print(school,"is a coding school")
+print(x + y)
+print("The product of 39 and 2 is", k*m)
