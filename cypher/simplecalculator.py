c223621d6fdb4f41cbe285e68327969ac4e302e4
VEXALIA <ruoroabigael@gmail.com>
2025-02-10 15:53:18 +0300

    Add files via upload

---
+++ cypher/simplecalculator.py
@@ -0,0 +1,21 @@
+# a program to device how a simplecalcutor work
+#- + /*
+num1 = int(input("enter the first number"))
+num2 = float(input("enter the second number"))
+
+print(num1,num2)
+print(num1 + num2)
+
+
+print(num1,num2)
+print(num1 - num2)
+
+
+
+print(num1,num2)
+print(num1*num2)
+
+
+print(num1,num2)
+print(num1 / num2)
+
