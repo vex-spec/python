761fc1b1266d4fc996bfbd9ce19728cd1bd980da
VEXALIA <ruoroabigael@gmail.com>
2025-02-10 14:24:12 +0300

    Add files via upload

---
+++ cypher/exceptionhandling.py
@@ -0,0 +1,15 @@
+try :
+    a = 50
+    b = 2
+    print(a / b)
+
+except :
+    print("Oops!")
+
+try :
+    # name ="Mary
+    print(name)
+except:
+    print("An error ocuried")
+finally:
+    print("Thank you")
